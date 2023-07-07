2d84a1a151154597ae9c029919185b44fb2df0cb
Nazmus Sakib <nazmus767921@gmail.com>
2023-07-07 17:38:11 +0600

    #

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { navLinks } from "../../Constants/Constant";
-import { Brand } from "../../assets/Images/Img";
+import { Brand, hamBurger } from "../../assets/Images/Img";
 const Nav = () => {
+	const [mobileMenu, setMobileMenu] = useState(false);
+
+	const handleMobileMenu = () => {
+		setMobileMenu((prev) => !prev);
+	};
+
 	return (
 		<>
 			<nav>
@@ -9,7 +16,7 @@ const Nav = () => {
 						<div className="nav_logo flex items-center w-[17%]">
 							<img src={Brand} alt="logo" />
 						</div>
-						<div className="nav_menu">
+						<div className="nav_menu lg:flex sm:hidden ">
 							<ul className="nav_link flex justify-around items-center text-lg">
 								{navLinks.map((link) => (
 									<li className="mx-4 my-1" key={link.id}>
@@ -18,6 +25,30 @@ const Nav = () => {
 								))}
 								<button className="btn">Get Started</button>
 							</ul>
+						</div>
+						<div
+							className="nav_mobile lg:hidden"
+							onClick={() => {
+								handleMobileMenu();
+							}}>
+							<div className="ham_burger w-5 relative z-10 m-2 focus:ring-2">
+								<img src={hamBurger} alt="mobile-menu" />
+							</div>
+							<div
+								className={`${
+									mobileMenu ? "flex" : "hidden"
+								} nav_menu--mobile`}>
+								<ul className="nav_link justify-start items-center text-xl flex flex-col w-[65%] h-screen fixed right-0 top-0 bg-slate-200">
+									{navLinks.map((link) => (
+										<li
+											className="mx-auto my-8 border-b-2 border-black"
+											key={link.id}>
+											<a href={link.link}>{link.name}</a>
+										</li>
+									))}
+									<button className="btn">Get Started</button>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>
--- src/assets/Images/Img.js
+++ src/assets/Images/Img.js
@@ -1,5 +1,6 @@
 import heroImg from "./hero_image-jpeg2x.jpeg";
 import Brand from "./best-homeopathic-treatment-center-naturopathic-treatment-logo_color.svg";
 import heroLeaf from "./best-homeopathic-treatment-center-naturopathic-treatment-leaf.png";
+import hamBurger from "./ham-burger.svg";
 
-export { Brand, heroImg, heroLeaf };
+export { Brand, heroImg, heroLeaf, hamBurger };
