1092d79c497b9760b010e09f52208b345e5e6e38
Nazmus Sakib <nazmus767921@gmail.com>
2023-07-09 19:17:29 +0600

    #

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Hero from "./Components/Hero/Hero";
 import Nav from "./Components/Nav/Nav";
-import Test from "./Components/Test/test";
+// import Test from "./Components/Test/test";
 
 //! default function
 function App() {
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 //! Imports
 import { useState } from "react";
 import { navLinks } from "../../Constants/Constant";
