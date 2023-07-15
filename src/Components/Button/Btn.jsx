29a3474b7ba231ec95cc02d81adf8cac032784a5
Nazmus Sakib <nazmus767921@gmail.com>
2023-07-15 15:12:21 +0600

    #

--- src/Components/Button/Btn.jsx
+++ src/Components/Button/Btn.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
-export const Btn = (props) => {
-	const { className, name, link } = props;
+export const Btn = ({ className, name, link }) => {
 	return (
 		<a href={link}>
 			<button className={`btn ${className}`}>{name}</button>
